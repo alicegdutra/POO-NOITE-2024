5a6cff6b65ed2c916d19837c47aa9bb5c98b6cfb
Alice <alicegzdutra@gmail.com>
2024-03-11 21:48:35 -0300

    LETRA

--- Encapsulamento/Conta.cs
+++ Encapsulamento/Conta.cs
@@ -7,6 +7,43 @@ namespace Encapsulamento
 {
     public class Conta
     {
-        
+        private int numero; //minúsculo - ATRIBUTO
+        private string? titular;
+        private double saldo;
     }
+    public int Numero //maiúsculo - PROPRIEDADE
+    {
+        set{
+            this.Numero = value; //alterar
+            //recebe qualquer valor, é genérico
+            //armazena/ altera no atributo NUMERO
+            // THIS - nome de instância genérico, referência qualquer nome de variável
+        }
+        get {
+            return this.Numero; //buscar
+        }
+
+    }
+    /*public void SetNumero(int numeroConta) //parametro para alteração EXEMPLO 1
+    {
+        numero = numeroConta;
+        //sair um valor de uma função - retorno
+        //estado do objeto - valor que a variável tem
+
+    }
+    public int GetNumero()
+    {
+        return numero;
+    }
+    public void AlterarNumero(int numeroConta) //parametro para alteração EXEMPLO 2
+    {
+       numero = numeroConta;
+        //sair um valor de uma função - retorno
+        //estado do objeto - valor que a variável tem
+
+    }
+    public int BuscarNumero()
+    {
+       return numero;
+    //}*/
 }
--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Encapsulamento;
+
+Conta c1 = new Conta();
+/*PADRÃO C# PARA ENCAPSULAMENTO - ALTERAÇÃO DE CONTEÚDO */
+c1.Numero = 1000; //set
+Console.WriteLine("Numero" = c1.Numero()); //get
+
+
+/*c1.AlterarNumero(1000); EXEMPLO 1
+Console.WriteLine("Numero" = c1.BuscarNumero()); */
+
+/*c1.SetNumero(1000); EXEMPLO 2
+Console.WriteLine("Numero" = c1.GetNumero()); */
+
