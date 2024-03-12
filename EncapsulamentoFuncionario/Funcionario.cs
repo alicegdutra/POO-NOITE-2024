a0b95d1fc5459c08a178ea293ca919c684341787
Alice <alicegzdutra@gmail.com>
2024-03-11 22:07:26 -0300

    funcionario

---
+++ EncapsulamentoFuncionario/Funcionario.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoFuncionario
+{
+    public class Funcionario
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        
+    }
+}
---
+++ EncapsulamentoFuncionario/Program.cs
@@ -0,0 +1,5 @@
+﻿using EncapsulamentoFuncionario;
+
+Funcionario f1 = new Funcionario();
+f1.Nome = Alice; //set
+Console.WriteLine("Nome" = f1.Nome()); //get
