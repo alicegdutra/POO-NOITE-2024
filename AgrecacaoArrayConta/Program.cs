f4d492f46b55b61b91eb02154a88fe03c0313ada
alicegdutra <alicegzdutra@gmail.com>
2024-03-25 21:54:38 -0300

    commit

--- AgrecacaoArrayConta/Program.cs
+++ AgrecacaoArrayConta/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AgregacaoArrayConta;
+
+Conta c1 = new Conta();
+c1.Numero = 1;
+c1.Saldo = 100;
+c1.VetTitulares = new List<Cliente>();
+
+for (int i = 0; i < 3; i++)
+{
+    Cliente cli = new Cliente();
+    System.Console.WriteLine("Nome: ");
+    cli.Nome = Console.ReadLine();
+    System.Console.WriteLine("RG: ");
+    cli.Rg = Console.ReadLine();
+    c1.VetTitulares.Add(cli);
+}
+c1.Mostrar();
+foreach(Cliente c in c1.VetTitulares)
+    c.Mostrar();
+
